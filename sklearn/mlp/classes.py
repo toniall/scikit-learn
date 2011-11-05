4a993af12653a8a8f29e97be16a858393b26a91f
Andreas Mueller <amueller@ais.uni-bonn.de>
2011-11-05 18:15:12 +0100

    label transform inside MLPClassifier class

--- sklearn/mlp/classes.py
+++ sklearn/mlp/classes.py
@@ -1,5 +1,6 @@
 from ..base import ClassifierMixin
 from .base import BaseMLP
+from ..preprocessing import LabelBinarizer
 
 class MLPClassifier(BaseMLP, ClassifierMixin):
     """ Multilayer Perceptron Classifier.
@@ -23,3 +24,15 @@ class MLPClassifier(BaseMLP, ClassifierMixin):
 
     References
     ----------"""
+
+    def fit(self, X, y, max_epochs, shuffle_data):
+        self.lb = LabelBinarizer()
+        one_hot_labels = self.lb.fit_transform(y)
+        super(MLPClassifier, self).fit(
+                X, one_hot_labels, max_epochs,
+                shuffle_data)
+        return self
+
+    def predict(self, X):
+        prediction = super(MLPClassifier, self).predict(X)
+        return self.lb.inverse_transform(prediction)
