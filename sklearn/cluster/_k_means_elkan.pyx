5e493eb5f8f90c3e8c4dc384efdda4ff88bafc1a
Andreas Mueller <amueller@ais.uni-bonn.de>
2013-05-26 20:51:15 +0200

    more typing in elkan kmeans

--- sklearn/cluster/_k_means_elkan.pyx
+++ sklearn/cluster/_k_means_elkan.pyx
@@ -1,19 +1,22 @@
 import numpy as np
+cimport numpy as np
+cimport numpy as npc
+cimport cython
+
+from cpython cimport bool
 
 from ..metrics import euclidean_distances
 from .k_means_ import _tolerance
 from ._k_means import _centers_dense
 
 
-def d(a, b):
+cdef d(a, b):
     return np.sqrt(np.sum((a - b) ** 2))
 
 
-def assign_labels(X, centers, center_distances=None):
+cdef assign_labels(double[:, :] X, double[:, :] centers, double[:, :] center_distances):
     # assigns closest center to X
     # uses triangle inequality
-    if center_distances is None:
-        center_distances = euclidean_distances(centers) / 2.
     new_centers, distances = [], []
     for x in X:
         # assign first cluster center
@@ -32,21 +35,32 @@ def assign_labels(X, centers, center_distances=None):
     return np.array(new_centers, dtype=np.int32), np.array(distances)
 
 
-def k_means_elkan(X, n_clusters, init, float tol=1e-4, int max_iter=30, verbose=False):
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.cdivision(True)
+def k_means_elkan(X_, n_clusters, init, float tol=1e-4, int max_iter=30, verbose=False):
     #initialize
-    tol = _tolerance(X, tol)
-    centers = init
-    cdef int n_samples = X.shape[0]
+    tol = _tolerance(X_, tol)
+    cdef double[:, :] centers = init
+    cdef double[:, :] new_centers
+
+    cdef int n_samples = X_.shape[0]
+    cdef int n_features = X_.shape[1]
     cdef int n_centers = centers.shape[0]
-    cdef int point_index, center_index, label
-    cdef float upper_bound, distance
+    cdef int point_index, center_index, label, sample, feature
+    cdef float upper_bound, distance, inertia
     cdef double[:, :] center_distances = euclidean_distances(centers) / 2.
     cdef double[:, :] lower_bounds = np.zeros((n_samples, n_centers))
-    labels, upper_bounds = assign_labels(
-        X, centers, center_distances=center_distances)
-    # make bounds tight for current labelss
-    lower_bounds[np.arange(n_samples), labels] = upper_bounds
-    [:] bounds_tight = np.ones(n_samples, dtype=np.bool)
+    cdef double[:, :] X = X_
+    cdef double[:] center_shift
+    labels_, upper_bounds_ = assign_labels(X, centers,
+                                           center_distances)
+    cdef np.int32_t[:] labels = labels_
+    cdef float[:] upper_bounds = upper_bounds_
+    # make bounds tight for current labels
+    for sample in range(n_samples):
+        lower_bounds[sample, labels[sample]] = upper_bounds[sample]
+    cdef np.uint8_t[:] bounds_tight = np.ones(n_samples, dtype=np.uint8)
     for iteration in range(max_iter):
         distance_next_center = np.sort(center_distances, axis=0)[1]
         points_to_update = distance_next_center[labels] < upper_bounds
@@ -60,9 +74,9 @@ def k_means_elkan(X, n_clusters, init, float tol=1e-4, int max_iter=30, verbose=
                         and (upper_bound > center_distances[center_index, label])):
                     # update distance to center
                     if not bounds_tight[point_index]:
-                        upper_bound = d(X[point_index], centers[label])
+                        upper_bound = d(X[point_index], centers[label, :])
                         lower_bounds[point_index, label] = upper_bound
-                        bounds_tight[point_index] = True
+                        bounds_tight[point_index] = 1
                     # check for relabels
                     if (upper_bound > lower_bounds[point_index, center_index]
                             or (upper_bound > center_distances[label, center_index])):
@@ -76,20 +90,31 @@ def k_means_elkan(X, n_clusters, init, float tol=1e-4, int max_iter=30, verbose=
 
         # compute new centers
         new_centers = _centers_dense(X, labels, n_centers, upper_bounds)
-        bounds_tight = np.zeros(n_samples, dtype=np.bool)
+        bounds_tight = np.zeros(n_samples, dtype=np.uint8)
 
         # compute distance each center moved
-        center_shift = np.sqrt(np.sum((centers - new_centers) ** 2, axis=1))
+        center_shift = np.zeros((n_centers, n_features))
+        for center_index in range(n_centers):
+            for feature in range(n_features):
+                center_shift[center_index] += (centers[center_index, feature] - new_centers[center_index, feature]) ** 2
+            center_shift[center_index] = np.sqrt(center_shift[center_index])
         # update bounds accordingly
-        lower_bounds = np.maximum(lower_bounds - center_shift, 0)
-        upper_bounds = upper_bounds + center_shift[labels]
+        for sample_index in range(n_samples):
+            for cluster_index in range(n_clusters):
+                lower_bounds[sample_index, center_index] = np.max(lower_bounds[sample_index, center_index] - center_shift[center_index], 0)
+            upper_bounds[sample_index] = upper_bounds[sample_index] + center_shift[labels[sample_index]]
         # reassign centers
         centers = new_centers
         # update between-center distances
         center_distances = euclidean_distances(centers) / 2.
         if verbose:
+            inertia = 0
+            for point_index in range(n_samples):
+                for feature in range(n_features):
+                    inertia += np.sum((X[point_index, feature] - centers[labels[point_index], feature]) ** 2)
+
             print('Iteration %i, inertia %s'
-                  % (iteration, np.sum((X - centers[labels]) ** 2)))
+                  % (iteration, inertia))
 
         if np.sum(center_shift) < tol:
             print("center shift within tolerance")
