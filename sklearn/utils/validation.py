42d8daf9304306f6c5b270b4a7677c0f9076c2df
Andreas Mueller <amueller@nyu.edu>
2015-01-06 19:32:54 -0500

    try to fix what is happening to y (by doing nothing to y)

--- sklearn/utils/validation.py
+++ sklearn/utils/validation.py
@@ -211,8 +211,9 @@ def _ensure_sparse_format(spmatrix, accept_sparse, dtype, order, copy,
     return spmatrix
 
 
-def check_array(array, accept_sparse=None, dtype=None, order=None, copy=False,
-                force_all_finite=True, ensure_2d=True, allow_nd=False):
+def check_array(array, accept_sparse=None, dtype="numeric", order=None,
+                copy=False, force_all_finite=True, ensure_2d=True,
+                allow_nd=False):
     """Input validation on an array, list, sparse matrix or similar.
 
     By default, the input is converted to an at least 2nd numpy array.
@@ -230,9 +231,9 @@ def check_array(array, accept_sparse=None, dtype=None, order=None, copy=False,
         If the input is sparse but not in the allowed format, it will be
         converted to the first listed format.
 
-    dtype : string, type or None (default=none)
-        Data type of result. If None, the dtype of the input is preserved,
-        unless array.dtype is object.
+    dtype : string, type or None (default="numeric")
+        Data type of result. If None, the dtype of the input is preserved.
+        If "numeric", dtype is preserved unless array.dtype is object.
 
     order : 'F', 'C' or None (default=None)
         Whether an array will be forced to be fortran or c-style.
@@ -259,14 +260,19 @@ def check_array(array, accept_sparse=None, dtype=None, order=None, copy=False,
         accept_sparse = [accept_sparse]
 
     if sp.issparse(array):
+        if dtype == "numeric":
+            dtype = None
         array = _ensure_sparse_format(array, accept_sparse, dtype, order,
                                       copy, force_all_finite)
     else:
         if ensure_2d:
             array = np.atleast_2d(array)
-        if dtype is None and getattr(array, "dtype", None) is np.dtype(object):
-            # if no conversion is given, and input is object, convert to float.
-            dtype = np.float
+        if dtype == "numeric":
+            if getattr(array, "dtype", None) is np.dtype(object):
+                # if input is object, convert to float.
+                dtype = np.float
+            else:
+                dtype = None
         array = np.array(array, dtype=dtype, order=order, copy=copy)
         if not allow_nd and array.ndim >= 3:
             raise ValueError("Found array with dim %d. Expected <= 2" %
@@ -331,7 +337,7 @@ def check_X_y(X, y, accept_sparse=None, dtype=None, order=None, copy=False,
     X = check_array(X, accept_sparse, dtype, order, copy, force_all_finite,
                     ensure_2d, allow_nd)
     if multi_output:
-        y = check_array(y, 'csr', force_all_finite=True, ensure_2d=False)
+        y = check_array(y, 'csr', force_all_finite=True, ensure_2d=False, dtype=None)
     else:
         y = column_or_1d(y, warn=True)
         #if y.dtype is np.dtype(object):
